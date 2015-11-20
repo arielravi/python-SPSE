69560ba002fe1aee332478940e1eefefe7d28ec7
arielravi <aravi@rocketmail.com>
2015-11-20 10:52:42 -0500

    Modified logfile_usb

--- src/logfile_usb.py
+++ src/logfile_usb.py
@@ -11,8 +11,11 @@ print "Printing all lines with USB in it"
 
 
 for line in logfile.readlines() :
-		if line.lower().find("usb") != -1 :
-			print line
+	if line.lower().find("usb") != -1 :
+		print line
 
 
 print "Done"
+
+
+# example : sudo ./logfile_usb.py /var/log/syslog
